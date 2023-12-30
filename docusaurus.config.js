a53eea54a7f181555e97917a73905782daaab03d
lifewithmatthew <155180275+lifewithmatthew@users.noreply.github.com>
2023-12-30 10:39:28 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,13 +16,13 @@ const config = {
   url: 'https://lifewithmatthew.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/website/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'lifewithmatthew', // Usually your GitHub org/user name.
   projectName: 'website', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  deploymentBranch: 'main',
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
